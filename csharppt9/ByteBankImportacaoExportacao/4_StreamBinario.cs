85d5d1b37401252e5130d4acd7d20129ad780361
R0bs0n <71139106+R0bys0n@users.noreply.github.com>
2024-05-02 15:47:00 -0300

    Add files via upload

---
+++ csharppt9/ByteBankImportacaoExportacao/4_StreamBinario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class  Program
+    {
+        static void EscritaBinaria()
+        {
+            using (var fs = new FileStream("contaCorrente.txt", FileMode.Create))
+            using (var escritor = new BinaryWriter(fs))
+            {
+                escritor.Write(456); //Numero Agencia
+                escritor.Write(6265161); // Numero da Conta
+                escritor.Write(4500.50); //Saldo da conta
+                escritor.Write("Robyson Lima"); // Dono da conta
+            }
+        }
+
+        static void LeituraBinaria()
+        {
+            using (var fs = new FileStream("contaCorrente.txt", FileMode.Open))
+            using (var leitor = new BinaryReader(fs))
+            {
+                var agencia = leitor.ReadInt32();
+                var numero = leitor.ReadInt32();
+                var saldo = leitor.ReadDouble();
+                var titular = leitor.ReadString();
+
+                Console.WriteLine($"{agencia}/{numero} Nome :  {titular} Saldo : {saldo}");
+
+            }
+        }
+    }
+
+}
